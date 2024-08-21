3bd7fb5e9a9b05ed12db2b4b633fd60e92d9d65d
Muhammed Zübeyir <zubeyirkaratas@hotmail.com>
2024-08-21 20:30:35 +0300

    Kredi Kartı testi yapıldı.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -8,5 +8,7 @@ public class Merhaba {
         System.out.println("merhaba 2");
         System.out.println("task 1 kodları");
         System.out.println("Task 2 kodları");
+
+        System.out.println("task 3 kodları");
     }
 }
